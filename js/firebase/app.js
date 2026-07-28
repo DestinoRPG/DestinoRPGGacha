dead190199daaee5525600ca27f9a4208f5bf30c
DestinoRPG <destinorpgadell@gmail.com>
2026-07-28 23:09:31 +0200

    userservice nuevo

--- js/firebase/app.js
+++ js/firebase/app.js
@@ -2,19 +2,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/fireba
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyDjw2E3c4XMeNPJhCUeU6JIhs3KLHClvwc",
-        authDomain: "destino-rpg-gacha.firebaseapp.com",
-        projectId: "destino-rpg-gacha",
-        storageBucket: "destino-rpg-gacha.firebasestorage.app",
-        messagingSenderId: "335640128673",
-        appId: "1:335640128673:web:ae8609620fe49e9a8c54e0"
-    };
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "destino-rpg-gacha.firebaseapp.com",
+    projectId: "destino-rpg-gacha",
+    storageBucket: "destino-rpg-gacha.firebasestorage.app",
+    messagingSenderId: "335640128673",
+    appId: "1:335640128673:web:ae8609620fe49e9a8c54e0"
+};
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
