19b0ada478709e862f0f71040d6778aacecb426f
DestinoRPG <destinorpgadell@gmail.com>
2026-08-05 19:15:13 +0200

    consolelog

--- js/services/eventService.js
+++ js/services/eventService.js
@@ -1,21 +1,19 @@
-import { getCollection } from "../firebase/firestore.js";
-
-/**
- * Devuelve todos los eventos habilitados.
- */
 export async function getEnabledEvents() {
 
     const events = await getCollection("events");
 
+    console.log("EVENTOS LEIDOS:", events);
+
     return events.filter(event => event.enabled);
 }
 
-/**
- * Devuelve un evento por su ID.
- */
+
 export async function getEvent(id) {
 
     const events = await getCollection("events");
+
+    console.log("BUSCANDO EVENTO:", id);
+    console.log("EVENTOS DISPONIBLES:", events);
 
     return events.find(event => event.id === id) || null;
 }
