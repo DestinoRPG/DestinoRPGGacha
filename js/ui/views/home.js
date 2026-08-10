035b70c3ccd97410132f8f42f5fc1faca02a0581
DestinoRPG <destinorpgadell@gmail.com>
2026-08-11 00:51:11 +0200

    medio back

--- js/ui/views/home.js
+++ js/ui/views/home.js
@@ -106,7 +106,7 @@ export async function renderHome(profile) {
 
                 <section class="home-news">
 
-                    ${renderNews(profile)}
+                    ${renderNews()}
 
                 </section>
 
