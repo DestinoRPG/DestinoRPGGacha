cfc7e45919af0d315a4937518471aef78ab72a62
DestinoRPG <destinorpgadell@gmail.com>
2026-08-12 15:58:04 +0200

    rollback

--- js/ui/banner.js
+++ js/ui/banner.js
@@ -1,75 +1,56 @@
 import { canSummon } from "../services/rewardService.js";
 
-
 export function renderBanner(user) {
 
     return `
 
-        <section class="banner">
+<section class="banner">
 
-<div class="banner-space">
+    <div class="banner-space">
 
-    <div class="banner-stars"></div>
+        <div class="banner-stars"></div>
 
-    <div class="banner-orbit banner-orbit-1"></div>
-    <div class="banner-orbit banner-orbit-2"></div>
+        <div class="banner-orbit banner-orbit-1"></div>
+        <div class="banner-orbit banner-orbit-2"></div>
 
-    <div class="banner-portal">
-
-        <div class="banner-chain banner-chain-1"></div>
-        <div class="banner-chain banner-chain-2"></div>
-        <div class="banner-chain banner-chain-3"></div>
-
-        <div class="banner-lock"></div>
+        <div class="banner-core">
+            <span class="banner-core-symbol">✦</span>
+        </div>
 
     </div>
 
-    <span class="banner-spark banner-spark-1">✦</span>
-    <span class="banner-spark banner-spark-2">✦</span>
-    <span class="banner-spark banner-spark-3">✦</span>
-    <span class="banner-spark banner-spark-4">✦</span>
 
-</div>
+    <div class="banner-info">
 
-            </div>
+        <button
+            id="summonButton"
+            class="banner-button"
+            ${!canSummon(user) ? "disabled" : ""}
+        >
 
+            <span class="ticket-icon">
 
-            <div class="banner-info">
+                <span class="ticket-icon-hole"></span>
 
-                <button
-                    id="summonButton"
-                    class="banner-button"
-                    ${!canSummon(user) ? "disabled" : ""}
-                >
+            </span>
 
-                    <span class="key-icon">
+            <span class="banner-button-info">
 
-                        <span class="key-icon-ring"></span>
+                <span class="banner-button-main">
+                    INVOCAR
+                </span>
 
-                        <span class="key-icon-stem"></span>
+                <span class="banner-button-sub">
+                    1 TICKET
+                </span>
 
-                        <span class="key-icon-teeth"></span>
+            </span>
 
-                    </span>
+        </button>
 
+    </div>
 
-                    <span class="banner-button-info">
-
-                        <span class="banner-button-main">
-                            INVOCAR
-                        </span>
-
-                        <span class="banner-button-sub">
-                            1 LLAVE
-                        </span>
-
-                    </span>
-
-                </button>
-
-            </div>
-
-        </section>
+</section>
 
     `;
 
